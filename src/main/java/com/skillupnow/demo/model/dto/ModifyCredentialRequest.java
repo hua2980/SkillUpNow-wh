1220f02d101e268f561458900dccfec38ddf7fd2
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-04-20 19:12:39 -0700

    1. add alert for Internal Server Error (500) and printed the corresponding error message 2. add input validation rules for modify credential requests

--- src/main/java/com/skillupnow/demo/model/dto/ModifyCredentialRequest.java
+++ src/main/java/com/skillupnow/demo/model/dto/ModifyCredentialRequest.java
@@ -1,8 +1,21 @@
 package com.skillupnow.demo.model.dto;
 
+import com.skillupnow.demo.exception.ValidationGroups;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class ModifyCredentialRequest {
+
+  @NotEmpty(message = "username must not be empty", groups = {ValidationGroups.Update.class})
   private String username;
+
+  @NotEmpty(message = "password is required", groups = {ValidationGroups.Update.class})
+  @Size(min = 7, max = 20, message = "password must be between 7 and 20 characters", groups = {
+      ValidationGroups.Insert.class})
   private String newPassword;
+
+  @NotEmpty(message = "confirm password is required", groups = {ValidationGroups.Update.class})
   private String confirmPassword;
 
   public ModifyCredentialRequest() {
@@ -31,5 +44,11 @@ public class ModifyCredentialRequest {
 
   public void setConfirmPassword(String confirmPassword) {
     this.confirmPassword = confirmPassword;
+  }
+
+  @AssertTrue(message = "Password and confirm password must match", groups = {
+      ValidationGroups.Update.class})
+  private boolean isPasswordMatch() {
+    return newPassword.equals(confirmPassword);
   }
 }
