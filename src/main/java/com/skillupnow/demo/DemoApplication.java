b9698329db4707e2f6f9cd1b24d64720b96142f6
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-05-18 15:26:05 +0800

    modified the main entry point

--- src/main/java/com/skillupnow/demo/DemoApplication.java
+++ src/main/java/com/skillupnow/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,7 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaRepositories("com.skillupnow.demo.repository")
 @EntityScan("com.skillupnow.demo.model.po")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
   /**
    * Provides an instance of BCryptPasswordEncoder to be used as a bean throughout the application.
    *
@@ -34,6 +36,11 @@ public class DemoApplication {
    */
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(DemoApplication.class);
   }
 
 }
