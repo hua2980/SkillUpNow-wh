062c97e24a8d40a747b973a7533082f7f4ec0dc9
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-08-10 23:42:42 +0800

    solved problem with logout, minimize expiration time.

--- src/main/java/com/skillupnow/demo/security/SecurityConstants.java
+++ src/main/java/com/skillupnow/demo/security/SecurityConstants.java
@@ -7,7 +7,7 @@ package com.skillupnow.demo.security;
 public class SecurityConstants {
 
 	public static final String SECRET = "oursecretkey";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 18_000_00; // 30 minutes
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/signup";
